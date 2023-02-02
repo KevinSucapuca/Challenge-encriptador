6979a00b97544ca3e6827f3f8840ca271c3f7444
KevinSucapuca <ksucapuca007@gmail.com>
2023-02-01 22:56:22 -0500

    add script encriptar/desencriptar/copiar

---
+++ script.js
@@ -0,0 +1,80 @@
+
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+// `La letra "e" es convertida para "enter"`
+// `La letra "i" es convertida para "imes"`
+// `La letra "a" es convertida para "ai"`
+// `La letra "o" es convertida para "ober"`
+// `La letra "u" es convertida para "ufat"`
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = ""
+    mensaje.style.backgroundImage = "none"
+
+    
+
+}
+
+
+
+function encriptar(stringEncriptado) {
+
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringEncriptado = stringEncriptado.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+
+        if(stringEncriptado.includes(matrizCodigo[i][0])) {
+
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+        
+    }
+    return stringEncriptado;
+    
+}
+
+
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = ""
+    
+
+    
+
+}
+
+
+function desencriptar(stringDesencriptado) {
+
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringDesencriptado = stringDesencriptado.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+
+        if(stringDesencriptado.includes(matrizCodigo[i][1])) {
+
+            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+        
+    }
+    return stringDesencriptado;
+    
+}
+
+
+function copiar() {
+
+    var copiaTxt = document.getElementById("resultado");
+
+    copiaTxt.select();
+    copiaTxt.setSelectionRange(0, 99999);
+
+    navigator.clipboard.writeText(copiaTxt.value);
+
+}
